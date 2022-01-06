1f6f0d9a2fa06128d39137f12f071f581c450b67
蒼時弦也 <elct9620@frost.tw>
2022-01-06 17:10:20 +0800

    Add basic intraday/chart tests

---
+++ spec/fugle/intraday/chart_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe Fugle::Intraday::Chart do
+  subject(:chart) { described_class.new(data) }
+
+  let(:data) do
+    {
+      'o' => [1, 2, 1],
+      'h' => [2, 3, 2],
+      'l' => [1, 1, 1],
+      'c' => [1, 2, 2],
+      'v' => [100, 50, 90],
+      't' => %w[1641459374000 1641459374000 1641459374000]
+    }
+  end
+
+  describe '#to_json' do
+    subject { chart.to_json }
+
+    it { is_expected.not_to be_empty }
+    it { is_expected.to include('"open":1') }
+    it { is_expected.to include('"high":2') }
+    it { is_expected.to include('"low":1') }
+    it { is_expected.to include('"close":1') }
+    it { is_expected.to include('"volume":1') }
+    it { is_expected.to include('"time":"2022-01-06T16:56:14+08:00"') }
+  end
+end
