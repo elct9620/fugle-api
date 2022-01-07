ef227f4d35ee4f04c437a5f98b92f060246d760e
蒼時弦也 <elct9620@frost.tw>
2022-01-07 11:28:39 +0800

    Use JSON fixtures instead fake hash for intraday/chart test

--- spec/fugle/intraday/chart_spec.rb
+++ spec/fugle/intraday/chart_spec.rb
@@ -3,26 +3,17 @@
 RSpec.describe Fugle::Intraday::Chart do
   subject(:chart) { described_class.new(data) }
 
-  let(:data) do
-    {
-      'o' => [1, 2, 1],
-      'h' => [2, 3, 2],
-      'l' => [1, 1, 1],
-      'c' => [1, 2, 2],
-      'v' => [100, 50, 90],
-      't' => %w[1641459374000 1641459374000 1641459374000]
-    }
-  end
+  let(:data) { JSON.parse(File.read('spec/fixtures/intraday/chart.json')).dig('data', 'chart') }
 
   describe '#to_json' do
     subject { chart.to_json }
 
     it { is_expected.not_to be_empty }
-    it { is_expected.to include('"open":1') }
-    it { is_expected.to include('"high":2') }
-    it { is_expected.to include('"low":1') }
-    it { is_expected.to include('"close":1') }
-    it { is_expected.to include('"volume":1') }
+    it { is_expected.to include('"open":148.3') }
+    it { is_expected.to include('"high":148.75') }
+    it { is_expected.to include('"low":148.3') }
+    it { is_expected.to include('"close":148.65') }
+    it { is_expected.to include('"volume":168') }
     it { is_expected.to include('"time":') }
   end
 end
